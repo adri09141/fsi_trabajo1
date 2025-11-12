7aa64a97820b26b46a62e24e9714d4d82cce3e62
adri091410 <141488778+adri09141@users.noreply.github.com>
2025-11-12 10:14:39 +0000

    Update model.py

--- ensayo3/model.py
+++ ensayo3/model.py
@@ -21,6 +21,9 @@ class SimpleCNN(nn.Module):
         self.bn3 = nn.BatchNorm2d(32)
         self.conv4 = nn.LazyConv2d(out_channels=64, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(64)
+        
+        self.drop_conv = nn.Dropout2d(0.1) 
+        
         # 🔹 Clasificador
         self.gap = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.3) 
@@ -31,9 +34,13 @@ class SimpleCNN(nn.Module):
     def forward(self, x):
         # --- Cuerpo Conv ---
         x = self.conv1(x); x = self.bn1(x); x = self.relu(x); x = self.pool(x)
+        x = self.drop_conv(x)
         x = self.conv2(x); x = self.bn2(x); x = self.relu(x); x = self.pool(x)
+        x = self.drop_conv(x)
         x = self.conv3(x); x = self.bn3(x); x = self.relu(x); x = self.pool(x)
+        x = self.drop_conv(x)
         x = self.conv4(x); x = self.bn4(x); x = self.relu(x); x = self.pool(x)
+        x = self.drop_conv(x)
         # --- Clasificador ---
         x = self.gap(x)
         x = torch.flatten(x, 1) # Shape: [B, 64]
@@ -45,7 +52,7 @@ class SimpleCNN(nn.Module):
 # --- Instanciación ---
 model = SimpleCNN(num_classes=num_classes)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-3)
+optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-4)
 
 if __name__ == '__main__':
     print(f"\n Modelo CNN (Ultra-Ligero 16-32-32-64) con {num_classes} clases.")
