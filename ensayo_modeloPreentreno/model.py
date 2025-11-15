f31cf7d4574e6af6e03ec34b08eadfe88c758be5
adri091410 <141488778+adri09141@users.noreply.github.com>
2025-11-15 11:27:55 +0000

    Update and rename modelo.py to model.py

--- ensayo_modeloPreentreno/model.py
+++ ensayo_modeloPreentreno/model.py
@@ -10,8 +10,10 @@ weights = EfficientNet_B0_Weights.IMAGENET1K_V1  # Usa la versión más nueva
 model = efficientnet_b0(weights=weights)
 
 # Congelar todas las capas (para entrenar solo la última)
-for param in model.parameters():
+for name, param in model.named_parameters():
     param.requires_grad = False
+    if "features.7" in name or "features.8" in name or "classifier" in name:
+        param.requires_grad = True 
 
 # Reemplazar la capa final (fc) para adaptarla a tus clases
 num_classes = n_classes()  # función que ya tienes definida
@@ -20,7 +22,7 @@ model.classifier[1] = nn.Linear(in_features, num_classes)
 
 # Definir criterio y optimizador solo para la capa fc
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.AdamW(model.classifier[1].parameters(), lr=1e-3)
+optimizer = optim.AdamW(model.classifier[1].parameters(), lr=1e-4)
 
 if __name__ == '__main__':
     print(f"\nResNet50 preentrenado listo para entrenar con {num_classes} clases.")
