2229df47f10cbf320857dfbbe63ebd772308e4a7
Alba Ramos Quintana <albaramosquintana@gmail.com>
2025-11-16 01:13:21 +0000

    docstring

--- ensayo_modeloPreentreno/model.py
+++ ensayo_modeloPreentreno/model.py
@@ -1,3 +1,17 @@
+"""
+Configuración de EfficientNet-B0 preentrenado para clasificación
+del dataset ASL Alphabet.
+
+Este módulo incluye:
+
+· Carga de EfficientNet-B0 con pesos de ImageNet.
+· Congelamiento de todas las capas excepto los últimos bloques y la capa classifier.
+· Reemplazo de la capa final para adaptarla al número de clases del dataset.
+· Instanciación del modelo, junto con:
+    - La función de pérdida CrossEntropyLoss.
+    - El optimizador AdamW para la capa final.
+"""
+
 from dataset import *
 import torch
 from torch import nn, optim
@@ -6,21 +20,27 @@ from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 # ------------------------------------------------------------
 # Cargar modelo preentrenado (ResNet50 con pesos de ImageNet)
 # ------------------------------------------------------------
-weights = EfficientNet_B0_Weights.IMAGENET1K_V1  # Usa la versión más nueva
+weights = EfficientNet_B0_Weights.IMAGENET1K_V1 
 model = efficientnet_b0(weights=weights)
 
-# Congelar todas las capas (para entrenar solo la última)
+# -------------------------------------------------------------------
+# Congelar todas las capas excepto los últimos bloques y classifier
+# -------------------------------------------------------------------
 for name, param in model.named_parameters():
     param.requires_grad = False
     if "features.7" in name or "features.8" in name or "classifier" in name:
         param.requires_grad = True 
 
-# Reemplazar la capa final (fc) para adaptarla a tus clases
-num_classes = n_classes()  # función que ya tienes definida
+# ---------------------------------------------------
+# Reemplazar la capa final para el número de clases
+# ---------------------------------------------------
+num_classes = n_classes()  
 in_features = model.classifier[1].in_features
 model.classifier[1] = nn.Linear(in_features, num_classes)
 
-# Definir criterio y optimizador solo para la capa fc
+# ---------------------------------------------------------
+# Definir criterio y optimizador (solo para la capa final)
+# ---------------------------------------------------------
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.AdamW(model.classifier[1].parameters(), lr=1e-4)
 
